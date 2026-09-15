1ae12868d4c574af1ae1beaadf96714987440f1d
Steven Nguyen <icecream17.github@gmail.com>
2026-09-15 19:44:04 +0000

    part 2

--- src/parser.rs
+++ src/parser.rs
@@ -3,78 +3,105 @@
 //! A database is an input file, which may have commands to include other input files.
 //! Each file is further parsed into scopes and statements.
 //!
-//! If {`Token`}s are like Words, {{`SyntaxProduction`}}s are like the various kinds of increasingly complex
+//! If [`Token`]s are like Words, {{`SyntaxProduction`}}s are like the various kinds of increasingly complex
 //! phrases, sentences, paragraphs, pages, chapters, sections, volumes, and arcs that make up the story
 //! that is a database.
 //!
 //! If there is a {{`FileInclusion`}}, the lexer will stop, and load the {{`SyntaxProduction`}}s
 //! of that file in-place. It is currently unspecified, so the filepath of the {{`FileInclusion`}}
 //! will be relative to whatever is convenient, probably the current-working-directory.
+//!
+//! [`Token`]: Token
 
 use logos::Logos;
+use std::ops::Range;
+
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+struct Span(Range<usize>);
+
+#[derive(Default, Clone, PartialEq, Debug)]
+struct InvalidToken(String, Span);
 
 #[derive(Logos, Debug, PartialEq, Eq, Clone, Copy)]
+#[logos(error(InvalidToken, callback = |lex| InvalidToken(lex.slice().to_owned(), Span(lex.span()))))]
 #[logos(skip r"[[:space:]]+")] // Ignore ASCII whitespace
+/// A space separated unit of Metamath.
+///
+/// # Warning
+///
+/// There exists lexical ambiguity:
+/// (`Token`) enum variants are accepted as representing themselves OR any more general variant.
+///
+/// In other words, a specific token is allowed to be parsed in a general category.
+///
+/// See [`satisfies`].
+///
+/// [`satisfies`]: Token::satisfies
 enum Token {
-    #[token("$(", priority = 4)]
+    #[token("$(", priority = 5)]
     CommentStart,
 
-    #[token("$)", priority = 4)]
+    #[token("$)", priority = 5)]
     CommentEnd,
 
-    #[token("$[", priority = 4)]
+    #[token("$[", priority = 5)]
     FileInclusionStart,
 
-    #[token("$]", priority = 4)]
+    #[token("$]", priority = 5)]
     FileInclusionEnd,
 
     // metamath.pdf calls this a "block",
     // but "scope" is more intuitive imo.
-    #[token("${", priority = 4)]
+    #[token("${", priority = 5)]
     ScopeStart,
 
-    #[token("$}", priority = 4)]
+    #[token("$}", priority = 5)]
     ScopeEnd,
 
-    #[token("$c", priority = 4)]
+    #[token("$c", priority = 5)]
     ConstantDeclarationStart,
 
-    #[token("$v", priority = 4)]
+    #[token("$v", priority = 5)]
     VariableDeclarationStart,
 
-    #[token("$d", priority = 4)]
-    DVConditionStart,
+    #[token("$d", priority = 5)]
+    DvConditionStart,
 
-    #[token("$f", priority = 4)]
+    #[token("$f", priority = 5)]
     FloatingHypothesisStart,
 
-    #[token("$e", priority = 4)]
+    #[token("$e", priority = 5)]
     EssentialHypothesisStart,
 
-    #[token("$a", priority = 4)]
+    #[token("$a", priority = 5)]
     AxiomStart,
 
-    #[token("$p", priority = 4)]
+    #[token("$p", priority = 5)]
     ProofStart,
 
-    #[token("$=", priority = 4)]
+    #[token("$=", priority = 5)]
     ProofDetailsStart,
 
-    #[token("$.", priority = 4)]
+    #[token("$.", priority = 5)]
     ItemEnd,
 
-    #[token("?", priority = 4)]
-    MissingCompressedStep,
+    // Due to the combination of:
+    // 1. "AAA" cannot be three separate tokens as `Label` matches the whole thing
+    // 1. "A A" cannot be a single token because `MathSymbol` might need them to be separate
+    //
+    // we can only process compressed proofs in "chunks".
+    //
+    // # Possible interpretations
+    //
+    // - A valid number: `A`
+    // - An invalid number: `U`
+    // - Multiple numbers: `AA`
+    // - A combination of these: `AAUZ`
+    #[regex(r"[A-Z]+", priority = 4)]
+    CompressedChunkLabelCompatible,
 
-    // Thankfully, this does not capture "Z" in the midst of a word,
-    // see the `basic_lexing` test below.
-    #[token("Z", priority = 4)]
-    RecallCompressedStep,
-
-    // Instead of a callback which interrupts blazing-fast lexing,
-    // we convert this into a number upon actually parsing, for speed.
-    #[regex(r"[U-Y[:space:]]*[A-T]", priority = 3)]
-    CompressedStep,
+    #[regex(r"[A-Z?]+", priority = 3)]
+    CompressedChunkLabelIncompatible,
 
     #[regex(r"[A-Za-z0-9._\-]+", priority = 2)]
     Label,
@@ -82,8 +109,45 @@ enum Token {
     #[regex("[!-#%-~]+", priority = 1)]
     MathSymbol,
 
+    // This won't include "$)" because `CommentEnd` has higher priotity.
     #[regex("[!-~]+", priority = 0)]
     CommentPart,
+}
+
+impl Token {
+    /// Returns true if this token matches or can be interpreted/consumed as another token.
+    fn satisfies(self, expected: Self) -> bool {
+        self == expected
+            || matches!(
+                (self, expected),
+                (
+                    Self::CommentStart
+                        | Self::FileInclusionStart
+                        | Self::FileInclusionEnd
+                        | Self::ScopeStart
+                        | Self::ScopeEnd
+                        | Self::ConstantDeclarationStart
+                        | Self::VariableDeclarationStart
+                        | Self::DvConditionStart
+                        | Self::FloatingHypothesisStart
+                        | Self::EssentialHypothesisStart
+                        | Self::AxiomStart
+                        | Self::ProofStart
+                        | Self::ProofDetailsStart
+                        | Self::ItemEnd
+                        | Self::CompressedChunkLabelCompatible
+                        | Self::CompressedChunkLabelIncompatible
+                        | Self::Label
+                        | Self::MathSymbol,
+                    Self::CommentPart // Everything except CommentEnd
+                ) | (
+                    Self::CompressedChunkLabelCompatible
+                        | Self::CompressedChunkLabelIncompatible
+                        | Self::Label,
+                    Self::MathSymbol
+                ) | (Self::CompressedChunkLabelCompatible, Self::Label)
+            )
+    }
 }
 
 #[cfg(test)]
@@ -93,16 +157,30 @@ mod lexing_tests {
     #[test]
     fn basic_lexing() {
         assert_eq!(
-            Token::lexer("$( hi! $. $(( Z U\n A Zap").collect::<Vec<_>>(),
+            Token::lexer("$( hi! $. $(( ZU\n AAU Zap").collect::<Vec<_>>(),
             vec![
-                Ok(Token::CommentStart),         // $(
-                Ok(Token::MathSymbol),           // hi!
-                Ok(Token::ItemEnd),              // $.
-                Ok(Token::CommentPart),          // $((
-                Ok(Token::RecallCompressedStep), // Z
-                Ok(Token::CompressedStep),       // U\n A
-                Ok(Token::Label),                // Zap
+                Ok(Token::CommentStart),                   // $(
+                Ok(Token::MathSymbol),                     // hi!
+                Ok(Token::ItemEnd),                        // $.
+                Ok(Token::CommentPart),                    // $((
+                Ok(Token::CompressedChunkLabelCompatible), // ZU
+                Ok(Token::CompressedChunkLabelCompatible), // AAU
+                Ok(Token::Label),                          // Zap
             ]
-        )
+        );
+    }
+
+    #[test]
+    fn invalid_lex() {
+        assert_eq!(
+            Token::lexer("miku miku biiiimu！ \n[...]").collect::<Vec<_>>(),
+            vec![
+                Ok(Token::Label),
+                Ok(Token::Label),
+                Ok(Token::Label),
+                Err(InvalidToken(String::from("！"), 17..20)),
+                Ok(Token::MathSymbol),
+            ]
+        );
     }
 }
